af628b74bf4bec5bc3c4c783b3f7a4e7cba496a2
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-09-07 00:51:37 -0300

    vue-test-master - Primeiros passos

--- vue-test-master/tests/unit/Lance.spec.js
+++ vue-test-master/tests/unit/Lance.spec.js
@@ -3,5 +3,31 @@ import { mount } from '@vue/test-utils'
 
 test('não aceita lance com valor menor que zero', () => {
     const wrapper = mount(Lance)
-    expect(wrapper).toBeTruthy()
+    const input = wrapper.find('input')
+    input.setValue(-100)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    expect(lancesEmitidos).toBeUndefined()
+})
+
+test('emite um lance quando um valor é maior que zero', () => {
+    const wrapper = mount(Lance)
+    const input = wrapper.find('input')
+    input.setValue(100)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    expect(lancesEmitidos).toHaveLength(1)
+})
+
+test('emit o valor esperado de um lance válido', () => {
+    const wrapper = mount(Lance)
+    const input = wrapper.find('input')
+    input.setValue(100)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    // [
+    //     [100]
+    // ]
+    const lance = parseInt(lancesEmitidos[0][0])
+    expect(lance).toBe(100)
 })
