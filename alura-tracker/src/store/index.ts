90c4fbf12a9058e99a5267a73e9592fc4bc37f0c
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-10-13 11:28:20 -0300

    alura-tracker - Evoluindo o Alura Tracker

--- alura-tracker/src/store/index.ts
+++ alura-tracker/src/store/index.ts
@@ -1,28 +1,36 @@
-import { InjectionKey } from 'vue'
-import { createStore, Store, useStore as vuexUseStore } from "vuex";
+import {InjectionKey} from 'vue'
+import {createStore, Store, useStore as vuexUseStore} from "vuex";
 import IProjeto from "@/interfaces/IProjeto"
+import {ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUIR_PROJETO} from "@/store/tipo-mutacoes";
 
 interface Estado {
-  projetos: IProjeto[]
+    projetos: IProjeto[]
 }
 
 export const key: InjectionKey<Store<Estado>> = Symbol()
 
 export const store = createStore<Estado>({
-  state: {
-    projetos: []
-  },
-  mutations: {
-    'ADICIONA_PROJETO'(state, nomdeDoProjeto: string){
-      const projeto = {
-        id: new Date().toISOString(),
-        nome: nomdeDoProjeto
-      } as IProjeto
-      state.projetos.push(projeto)
+    state: {
+        projetos: []
+    },
+    mutations: {
+        [ADICIONA_PROJETO](state, nomdeDoProjeto: string) {
+            const projeto = {
+                id: new Date().toISOString(),
+                nome: nomdeDoProjeto
+            } as IProjeto
+            state.projetos.push(projeto)
+        },
+        [ALTERA_PROJETO](state, projeto: IProjeto) {
+            const index = state.projetos.findIndex(proj => proj.id === projeto.id)
+            state.projetos[index] = projeto;
+        },
+        [EXCLUIR_PROJETO](state, id: string){
+            state.projetos = state.projetos.filter(proj => proj.id !== id)
+        }
     }
-  }
 })
 
 export function useStore(): Store<Estado> {
-  return vuexUseStore(key)
+    return vuexUseStore(key)
 }
--- alura-tracker/src/views/Projetos.vue
+++ alura-tracker/src/views/Projetos.vue
@@ -1,71 +1,18 @@
 <template>
-  <section class="projetos">
+  <div class="projetos">
     <h1 class="title">Projetos</h1>
-    <form @submit.prevent="salvar">
-      <div class="field">
-        <label
-          for="nomeDoProjeto"
-          class="label"
-        >Nome do projeto</label>
-        <input
-          type="text"
-          class="input"
-          id="nomeDoProjeto"
-          v-model="nomeDoProjeto"
-        />
-      </div>
-      <div class="field">
-        <button
-          class="button"
-          type="submit"
-        >Salvar</button>
-      </div>
-    </form>
-    <table class="table is-fullwidth mt-5">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nome</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="projeto in projetos"
-          :key="projeto.id"
-        >
-          <td>{{ projeto.id }}</td>
-          <td>{{ projeto.nome }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </section>
+    <router-view></router-view>
+  </div>
 </template>
+
 <script lang="ts">
-import {computed, defineComponent} from "vue";
-import {useStore} from "@/store";
+import {defineComponent} from "vue";
 
 export default defineComponent({
-  name: "Projetos",
-  data() {
-    return {
-      nomeDoProjeto: "",
-    };
-  },
-  methods: {
-    salvar() {
-      this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
-      this.nomeDoProjeto = "";
-    },
-  },
-  setup(){
-    const store = useStore();
-    return {
-      store,
-      projetos: computed(() => store.state.projetos)
-    }
-  }
-});
+  name: 'Projetos'
+})
 </script>
+
 
 <style scoped>
 .projetos {
